20c784ebfe59b88ce928804cb04966a7df56d3ed
rizwan <rizwanghanchi2808@gmail.com>
2021-07-25 17:48:45 +0530

    configure for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ console.log("javascript is up and running")
 
 const getWeatherDetails=(location)=>{
     
-fetch('http://localhost:3000/weather?address='+location).then((res)=>{
+fetch('/weather?address='+location).then((res)=>{
     res.json().then((data)=>{
         if(data.error){
             console.log(data.error)
